15ce4e59e41dcb264a6dca830db2a79c70a75991
Aamir Hafiez <aamirhafiez@gmail.com>
2025-06-29 21:32:50 +0530

    chore: Changed README.md

--- config/AppConfig.ts
+++ config/AppConfig.ts
@@ -1,4 +1,4 @@
 export default Object.freeze({
-  NETWORK_BASE_URL: "http://192.168.1.3:8092",
+  NETWORK_BASE_URL: "http://[add-ip-here]:8092",
   GENERIC_ERROR_STRING: "Oops!, Something went wrong.",
 });
